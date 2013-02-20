0c09e8cccccf5ea508ca796085170a6e28979c69
Victor Olvera-Smith <victor_olvera-smith@dell.com>
2013-02-20 16:16:56 +0000

    Adding background

--- src/com/example/srfc/SRFCActivity.java
+++ src/com/example/srfc/SRFCActivity.java
@@ -2,6 +2,7 @@ package com.example.srfc;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class SRFCActivity extends Activity
 {
@@ -9,7 +10,8 @@ public class SRFCActivity extends Activity
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.custom_title);
     }
 }
